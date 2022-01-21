ec73fb47e7cac8b3e80655c34cd769a1d595906f
pavel.shvedov <composer.shvedov@gmail.com>
2022-01-21 13:29:42 +0300

    Initial commit

---
+++ src/Var.java
@@ -0,0 +1,9 @@
+public class Var {
+    public static void main(String[] args) {
+        int days = 1000;
+        int speed = 300000;
+        long seconds = days * 24 * 60 * 60;
+        long distance = speed * seconds;
+        System.out.println(distance);
+    }
+}
